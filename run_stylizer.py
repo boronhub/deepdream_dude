8a3a56622ba616b632b41a88028761b4a70add5e
boron <31139873+boronhub@users.noreply.github.com>
2019-10-16 16:34:12 +0530

    Update run_stylizer.py

--- run_stylizer.py
+++ run_stylizer.py
@@ -1,12 +1,12 @@
 import os
 import argparse
 
-myPath = 'E:/Dhruv/Projects_19-20/DeepDream/deep_dreaming_start/output'
+myPath = 'your-path-here'
 myFiles = os.listdir(myPath)
-os.system('cd ‪E:/Dhruv/Projects_19-20/DeepDream/deep_dreaming_start')
+os.system('your-path-here')
 for f in myFiles:
     img_in = myPath + "/" + f
     parser = argparse.ArgumentParser(description=None)
     args = parser.parse_args()
-    cmd_run = f"python neural_style.py --content {img_in} --styles content_style.jpg --output E:/Dhruv/Projects_19-20/DeepDream/deep_dreaming_start/output/final/output_{f}.jpg --iterations 250"
+    cmd_run = f"python neural_style.py --content {img_in} --styles content_style.jpg --output your-path-here/output/final/output_{f}.jpg --iterations 250"
     os.system(cmd_run)
