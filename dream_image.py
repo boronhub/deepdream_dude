704abd0a3d3ed9586c3974010d14a6987c79698f
boron <31139873+boronhub@users.noreply.github.com>
2019-10-16 16:49:06 +0530

    Update dream_image.py

--- dream_image.py
+++ dream_image.py
@@ -1,26 +1,12 @@
 '''
-Some info on various layers, so you know what to expect
-depending on which layer you choose:
-
 layer 1: wavy
 layer 2: lines
 layer 3: boxes
-layer 4: circles?
-layer 6: dogs, bears, cute animals.
+layer 4: circles
+layer 6: dogs, bears, cute animals
 layer 7: faces, buildings
-layer 8: fish begin to appear, frogs/reptilian eyes.
-layer 10: Monkies, lizards, snakes, duck
-
-Choosing various parameters like num_iterations, rescale,
-and num_repeats really varies on which layer you're doing.
-
-
-We could probably come up with some sort of formula. The
-deeper the layer is, the more iterations and
-repeats you will want.
-
-Layer 3: 20 iterations, 0.5 rescale, and 8 repeats is decent start
-Layer 10: 40 iterations and 25 repeats is good. 
+layer 8: fish begin to appear, trippyEyes
+layer 10: bandars, chipkalis, saanps
 '''
 
 from deepdreamer import model, load_image, recursive_optimize
